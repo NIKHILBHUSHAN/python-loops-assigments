1bc6b18ba79ce7b8aa258e7b547929c47400c4fb
NIKHILBHUSHAN <nikhilbhushan95824@gmail.com>
2026-02-22 19:19:44 +0530

    Task 2: Array Shape and Statistics

---
+++ scores.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+scores=np.array([85, 90, 78, 92, 88, 76, 95, 82, 89, 91, 87, 84])
+
+print("Shape:",scores.shape)
+
+print("Total elements:",scores.size)
+
+print("Highest score:",scores.max())
+
+print("Lowest score:",scores.min())
+
+print("Range:",scores.max()-scores.min())
+
